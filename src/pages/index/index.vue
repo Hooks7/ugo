84a4d72a40c3beb3c44665bfcd443bf8cfd123a9
Hooks7 <youremail@example.com>
2019-09-28 20:40:40 +0800

    回到顶部设置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -25,16 +25,17 @@
     <div class="floors">
       <div class="floor" v-for="(item,index) in floor" :key="index">
         <div class="title">
-          <img :src="item.floor_title.image_src"/>
+          <img :src="item.floor_title.image_src" />
         </div>
         <div class="items">
-          <a href v-for="(e,key) in item.product_list" :key="key"> 
+          <a href v-for="(e,key) in item.product_list" :key="key">
             <img :src="e.image_src" />
           </a>
         </div>
       </div>
-     
     </div>
+    <!-- 回到顶部 -->
+    <div class="top" @click="goTop" v-if="scrollTop>180"></div>
   </div>
 </template>
 
@@ -51,7 +52,9 @@ export default {
       // 首页分页
       pageSplit: [],
       // 楼层数据
-      floor:[],
+      floor: [],
+      // 回到顶部
+      scrollTop: ""
     };
   },
 
@@ -85,6 +88,12 @@ export default {
         url: "api/public/v1/home/floordata"
       });
       this.floor = message;
+    },
+
+    goTop() {
+      // 通过 api 可以设置页面的滚动高度
+      // 当滚动高度为 0 时，即为回到顶部
+      mpvue.pageScrollTo({ scrollTop: 0 });
     }
   },
 
@@ -92,6 +101,9 @@ export default {
     this.getSlideshow();
     this.homepageSplit();
     this.getFloor();
+  },
+  onPageScroll(e) {
+    this.scrollTop = e.scrollTop;
   }
 };
 </script>
@@ -147,5 +159,15 @@ export default {
       margin-right: 10rpx;
     }
   }
+}
+.top {
+  background-color: rgb(255, 0, 55);
+  border-radius: 50%;
+  opacity: 0.3;
+  width: 100rpx;
+  height: 100rpx;
+  position: fixed;
+  right: 25rpx;
+  bottom: 50rpx;
 }
 </style>
