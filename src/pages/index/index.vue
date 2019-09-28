ff74c373701ed4f5f47f946ab974a8eb24604edb
Hooks7 <youremail@example.com>
2019-09-28 21:42:16 +0800

    转发设置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -16,8 +16,16 @@
       </swiper-item>
     </swiper>
     <!-- 导航条 -->
+    <!-- 使用 navigator 跳转页面时，需要指定新页面打开的方式 -->
+    <!-- 即通过 open-type 属性设置 -->
+    <!-- 如果跳转的页面为 tabBar open-type 值只能为 switchTab -->
     <div class="navs">
-      <a href v-for="(item,index) in pageSplit" :key="index">
+      <a
+        :href="item.navigator_url ||'/pages/list/main?query=' + item.name"
+        :open-type="item.open_type || 'navigate'"
+        v-for="(item,index) in pageSplit"
+        :key="index"
+      >
         <img :src="item.image_src" />
       </a>
     </div>
@@ -28,7 +36,11 @@
           <img :src="item.floor_title.image_src" />
         </div>
         <div class="items">
-          <a href v-for="(e,key) in item.product_list" :key="key">
+          <a
+            v-for="(e,key) in item.product_list"
+            :key="key"
+            :href="'/pages/list/main?query=' + e.name"
+          >
             <img :src="e.image_src" />
           </a>
         </div>
@@ -89,7 +101,7 @@ export default {
       });
       this.floor = message;
     },
-
+    // 回到顶部
     goTop() {
       // 通过 api 可以设置页面的滚动高度
       // 当滚动高度为 0 时，即为回到顶部
@@ -102,8 +114,28 @@ export default {
     this.homepageSplit();
     this.getFloor();
   },
+
+  // 监听页面的滚动
   onPageScroll(e) {
     this.scrollTop = e.scrollTop;
+  },
+
+  // 用户下拉
+  async onPullDownRefresh() {
+    await this.getSlideshow();
+    await this.homepageSplit();
+    await this.getFloor();
+
+    mpvue.stopPullDownRefresh();
+  },
+  // 转发触发回调
+  onShareAppMessage() {
+    return {
+      title: "和我一起学习小程序",
+      path: "/pages/index/main",
+      imageUrl:
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569672080944&di=4096b3b19485ca0fa468c9c9fac258ae&imgtype=0&src=http%3A%2F%2Fwww.999zx.cn%2Fadm_file%2Ffck%2Fimages%2F2018%2F4%2FImage%2F20184386447866.jpg"
+    };
   }
 };
 </script>
